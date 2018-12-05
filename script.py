19f7b6c44ee6fad99ee8f9a8e319690751e9c023
Tiago <riskybomberpt@gmail.com>
2018-12-05 16:49:32 +0000

    Add Initial CameraDetection class
    Add Initial CameraDetection class.
    This is still a work in progress based
    on a Computer Vision (OpenCV) example

--- script.py
+++ script.py
@@ -10,6 +10,10 @@ import ssl
 import jwt
 import paho.mqtt.client as mqtt
 
+import numpy as np
+import argparse #TODO this is temporary
+import cv2
+
 # Settings for the user to change
 serialPort = 'COM3'
 frequencyLogfile = 60 # Number of entry records per minute (at equal intervals)
@@ -327,8 +331,102 @@ class DataProcessing():
                                 payload = payload + row
                 return payload
 
+class CameraDetection():
+        COLORS = None
+        classes = None
 
-                        
+        def main(self):
+
+                ## TODO remove args and turn them into user configurations!
+                ap = argparse.ArgumentParser()
+                ap.add_argument('-i', '--image', required=True,
+                                help = 'path to input image')
+                ap.add_argument('-c', '--config', required=True,
+                                help = 'path to yolo config file')
+                ap.add_argument('-w', '--weights', required=True,
+                                help = 'path to yolo pre-trained weights')
+                ap.add_argument('-cl', '--classes', required=True,
+                                help = 'path to text file containing class names')
+                args = ap.parse_args()
+
+
+                image = cv2.imread(args.image)
+
+                Width = image.shape[1]
+                Height = image.shape[0]
+                scale = 0.00392
+
+                self.classes = None
+
+                with open(args.classes, 'r') as f:
+                        self.classes = [line.strip() for line in f.readlines()]
+
+                self.COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
+
+                net = cv2.dnn.readNet(args.weights, args.config)
+
+                blob = cv2.dnn.blobFromImage(image, scale, (416,416), (0,0,0), True, crop=False)
+
+                net.setInput(blob)
+
+                outs = net.forward(self.get_output_layers(net))
+
+                class_ids = []
+                confidences = []
+                boxes = []
+                conf_threshold = 0.5
+                nms_threshold = 0.4
+
+
+                for out in outs:
+                        for detection in out:
+                                scores = detection[5:]
+                                class_id = np.argmax(scores)
+                                confidence = scores[class_id]
+                                if confidence > 0.5:
+                                        center_x = int(detection[0] * Width)
+                                        center_y = int(detection[1] * Height)
+                                        w = int(detection[2] * Width)
+                                        h = int(detection[3] * Height)
+                                        x = center_x - w / 2
+                                        y = center_y - h / 2
+                                        class_ids.append(class_id)
+                                        confidences.append(float(confidence))
+                                        boxes.append([x, y, w, h])
+
+
+                indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+
+                for i in indices:
+                        i = i[0]
+                        box = boxes[i]
+                        x = box[0]
+                        y = box[1]
+                        w = box[2]
+                        h = box[3]
+                        self.draw_prediction(image, class_ids[i], confidences[i], round(x), round(y), round(x+w), round(y+h))
+
+                cv2.imshow("object detection", image)
+                cv2.waitKey()
+
+                cv2.imwrite("object-detection.jpg", image)
+                cv2.destroyAllWindows()
+
+        def get_output_layers(self, net):
+                layer_names = net.getLayerNames()
+                output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+                return output_layers
+        
+        def draw_prediction(self, img, class_id, confidence, x, y, x_plus_w, y_plus_h):
+                label = str(self.classes[class_id])
+                color = self.COLORS[class_id]
+                cv2.rectangle(img, (x,y), (x_plus_w,y_plus_h), color, 2)
+                cv2.putText(img, label, (x-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+
+
+## Thread classes
 class SerialThread(Thread):
  
     def __init__(self):
@@ -471,7 +569,15 @@ class GCPThread(Thread):
                 GCloudIOT().main()
 
 
+class CameraThread(Thread):
+        def __init__(self):
+                Thread.__init__(self)
+        
+        def run(self):
+                ##Camera stuff here
+                return None
 
+## Main routine
 if __name__ == '__main__':
         
         thread1 = SerialThread()
@@ -480,22 +586,27 @@ if __name__ == '__main__':
         thread2 = DataThread()
         thread2.setName('Thread 2')
 
-        thread4 = DetectHumanThread()
+        thread3 = DetectHumanThread()
+        thread3.setName('Thread 3')
+
+        thread4 = CameraThread()
         thread4.setName('Thread 4')
 
         thread1.start()
         thread2.start()
-        thread4.start()
+        thread3.start()
+        #thread4.start() This is still in development
         
         thread1.join()
         thread2.join()
-        thread4.join()
+        thread3.join()
+        #thread4.join() This is still in development
 
         if mqtt_on:
-                thread3 = GCPThread()
-                thread3.setName('Thread 3')
-                thread3.start()
-                thread3.join()
+                thread5 = GCPThread()
+                thread5.setName('Thread 5')
+                thread5.start()
+                thread5.join()
         
         print('Main Terminating...')
 
