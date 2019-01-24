2fa6c120858eaa9001dc4337b6a68340db7d21b3
Tiago <riskybomberpt@gmail.com>
2019-01-24 10:53:47 +0000

    Dropping GCP support
    Drops GCP support as MQTT will be done later using
    other services
    
    Adds serial connection closing on exit

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # D6T8L06
 
-This application will log the values read from the D6T8L06, attempt to detect people, and export the values and results to a CSV or through the Google API to the cloud. It can also detect people using a Camera (usb or Pi camera), which we are using as a test/control measure for development.
+This application will log the values read from the D6T8L06, attempt to detect people, and export the values and results to a CSV. It can also detect people using a Camera (usb or Pi camera), which we are using as a test/control measure for development.
 
 This is still a work-in-progress application.
 
@@ -10,11 +10,6 @@ Here's how to solve all your Python dependencies for this project:
 
     python3 -m pip install -r requirements.txt
 
-## Google IoT Core source
-
-My Google IoT Core MQTT code is based on https://github.com/GoogleCloudPlatform/python-docs-samples/blob/master/iot/api-client/mqtt_example/cloudiot_mqtt_example.py
-
-Many thanks to developers behind Google Cloud Platform (https://github.com/GoogleCloudPlatform) for their example code.
 
 ## OpenCV source
 
--- script.py
+++ script.py
@@ -2,12 +2,7 @@ from threading import Thread
 import time
 import serial
 import datetime
-import os
-import sys
 import random
-import ssl
-import jwt
-import paho.mqtt.client as mqtt
 import numpy as np
 import cv2
 from imutils.video import VideoStream 
@@ -21,7 +16,6 @@ serialPort = '/dev/ttyUSB0'
 ##Writing intervals
 # This is the waiting time between writing calls, in seconds
 pLogFile = 1 # Interval between logfile writes
-pMQTT = 12 # Interval between MQTT telemetry reports ()
 pCam = 6 # Interval between camera picture saving and detecting
 
 #Detection parameters
@@ -43,17 +37,13 @@ filePathDetail = "/var/www/html/logfile-detail.csv" # Full file path, properly e
 #Functionality setup
 debug = False # If this is enabled the script will output the values being read to the console
 mode = "full-detail" # Check "Modes" for details
-mqtt_on = False
 csv_on = True
 cam_on = False
 cam_mode = "usb" # "usb" to use a USB camera, "pi" to use Pi's camera
 
 ## Modes
 # "full-detail" mode
-# This means both the Detail and Normal .csv files are outputted and the MQTT telemetry will be Detail only
-
-# "mqtt-normal" mode
-# This mode will output mqtt in Normal level, however it will output both Detail and Normal level .csv files
+# This means both the Detail and Normal .csv files are outputted
 
 # "full-normal" mode
 # This mode will output both the MQTT telemetry and the .csv file in Normal level only.
@@ -70,7 +60,6 @@ cam_mode = "usb" # "usb" to use a USB camera, "pi" to use Pi's camera
 ## 2. Change the extension to .txt and run the Text Importing Assistant
 
 ##### End of Settings #####
-buffer = True if (pLogFile != pMQTT) else False
 valsDetail = [0] * 8
 dhLastSensorVals = [[0,0,0]]
 for i in range(7):
@@ -81,206 +70,9 @@ dhPresenceTemp = [0,0,0,0,0,0,0,0]
 camBoundariesX = ((0,1),(2,3),(4,5),(6,7),(8,9),(10,11),(12,13),(14,15))
 camBoundariesY = (0,1)
 dhCamPresence = [0,0,0,0,0,0,0,0]
-bufferList = []
 valPTAT = 0
 connected = False
 notKill = True
-
-class GCloudIOT():
-        # The initial backoff time after a disconnection occurs, in seconds.
-        minimum_backoff_time = 1
-
-        # The maximum backoff time before giving up, in seconds.
-        MAXIMUM_BACKOFF_TIME = 32
-
-        # Whether to wait with exponential backoff before publishing.
-        should_backoff = False
-
-        # Configuration
-        arg_project_id = 'iot-test-0001' #GCP Cloud Project ID
-        arg_registry_id = 'test-iot' #Cloud IoT Core registry NAME
-        arg_device_id = 'device-python' #Cloud IoT Core device NAME
-        arg_private_key_file = 'C:\\Users\\Tiago Cabral\\Desktop\\mqtt_python_test\\rsa_private.pem' #Path to private key file
-        arg_algorithm = 'RS256' #Encryption to generate JWT, RS256 or ES256 available
-        arg_cloud_region = 'europe-west1' #GCP Cloud Region
-        arg_ca_certs = 'C:\\Users\\Tiago Cabral\\Desktop\\mqtt_python_test\\roots.pem' #Path to CA root obtained from https://pki.google.com/roots.pem
-        arg_message_type = 'event' #Event (telemetry) or State(device state)
-        arg_mqtt_bridge_hostname = 'mqtt.googleapis.com' #MQTT bridge hostname
-        arg_mqtt_bridge_port = 8883 #MQTT bridge port, 8883 or 443 recommended
-        arg_jwt_expires_minutes = 20 #Expiration time, in minutes, for JWT tokens
-
-        # [START iot_mqtt_jwt]
-        def create_jwt(self, project_id, private_key_file, algorithm):
-
-                token = {
-                        # The time that the token was issued at
-                        'iat': datetime.datetime.utcnow(),
-                        # The time the token expires.
-                        'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
-                        # The audience field should always be set to the GCP project id.
-                        'aud': project_id
-                }
-
-                # Read the private key file.
-                with open(private_key_file, 'r') as f:
-                        private_key = f.read()
-
-                print('Creating JWT using {} from private key file {}'.format(
-                algorithm, private_key_file))
-
-                return jwt.encode(token, private_key, algorithm=algorithm)
-        # [END iot_mqtt_jwt]
-
-
-        # [START iot_mqtt_config]
-        def error_str(self, rc):
-                """Convert a Paho error to a human readable string."""
-                return '{}: {}'.format(rc, mqtt.error_string(rc))
-
-
-        def on_connect(self, unused_client, unused_userdata, unused_flags, rc):
-                """Callback for when a device connects."""
-                print('on_connect', mqtt.connack_string(rc))
-
-        # After a successful connect, reset backoff time and stop backing off.
-                self.should_backoff = False
-                self.minimum_backoff_time = 1
-
-
-        def on_disconnect(self, unused_client, unused_userdata, rc):
-                """Paho callback for when a device disconnects."""
-                print('on_disconnect', self.error_str(rc))
-
-                # Since a disconnect occurred, the next loop iteration will wait with
-                # exponential backoff.
-                self.should_backoff = True
-
-
-        def on_publish(self, unused_client, unused_userdata, unused_mid):
-                """Paho callback when a message is sent to the broker."""
-                print('on_publish')
-
-
-        def on_message(self, unused_client, unused_userdata, message):
-                """Callback when the device receives a message on a subscription."""
-                payload = str(message.payload)
-                print('Received message \'{}\' on topic \'{}\' with Qos {}'.format(
-                payload, message.topic, str(message.qos)))
-
-
-        def get_client(self,
-                project_id, cloud_region, registry_id, device_id, private_key_file,
-                algorithm, ca_certs, mqtt_bridge_hostname, mqtt_bridge_port):
-                """Create our MQTT client. The client_id is a unique string that identifies
-                this device. For Google Cloud IoT Core, it must be in the format below."""
-                client = mqtt.Client(
-                        client_id=('projects/{}/locations/{}/registries/{}/devices/{}'
-                                .format(
-                                project_id,
-                                cloud_region,
-                                registry_id,
-                                device_id)))
-
-                # With Google Cloud IoT Core, the username field is ignored, and the
-                # password field is used to transmit a JWT to authorize the device.
-                client.username_pw_set(
-                        username='unused',
-                        password=self.create_jwt(
-                        project_id, private_key_file, algorithm))
-
-                # Enable SSL/TLS support.
-                client.tls_set(ca_certs=ca_certs, tls_version=ssl.PROTOCOL_TLSv1_2)
-
-                # Register message callbacks. https://eclipse.org/paho/clients/python/docs/
-                # describes additional callbacks that Paho supports. In this example, the
-                # callbacks just print to standard out.
-                client.on_connect = self.on_connect
-                client.on_publish = self.on_publish
-                client.on_disconnect = self.on_disconnect
-                client.on_message = self.on_message
-
-                # Connect to the Google MQTT bridge.
-                client.connect(mqtt_bridge_hostname, mqtt_bridge_port)
-
-                # This is the topic that the device will receive configuration updates on.
-                mqtt_config_topic = '/devices/{}/config'.format(device_id)
-
-                # Subscribe to the config topic.
-                client.subscribe(mqtt_config_topic, qos=1)
-
-                return client
-        # [END iot_mqtt_config]
-
-
-        # [START iot_mqtt_run]
-        def main(self):
-
-                global pMQTT
-
-                # Publish to the events or state topic based on the flag.
-                sub_topic = 'events' if self.arg_message_type == 'event' else 'state'
-
-                mqtt_topic = '/devices/{}/{}'.format(self.arg_device_id, sub_topic)
-
-                jwt_iat = datetime.datetime.utcnow()
-                jwt_exp_mins = self.arg_jwt_expires_minutes
-                client = self.get_client(
-                        self.arg_project_id, self.arg_cloud_region, self.arg_registry_id, self.arg_device_id,
-                        self.arg_private_key_file, self.arg_algorithm, self.arg_ca_certs,
-                        self.arg_mqtt_bridge_hostname, self.arg_mqtt_bridge_port)
-
-                # Publish mesages to the MQTT bridge.
-                global notKill
-                while(notKill):
-                        # Process network events.
-                        client.loop()
-
-                        # Wait if backoff is required.
-                        if self.should_backoff:
-                        # If backoff time is too large, give up.
-                                if self.minimum_backoff_time > self.MAXIMUM_BACKOFF_TIME:
-                                        print('Exceeded maximum backoff time. Giving up.')
-                                        break
-
-                        # Otherwise, wait and connect again.
-                                delay = self.minimum_backoff_time + random.randint(0, 1000) / 1000.0
-                                print('Waiting for {} before reconnecting.'.format(delay))
-                                time.sleep(delay)
-                                self.minimum_backoff_time *= 2
-                                client.connect(self.arg_mqtt_bridge_hostname, self.arg_mqtt_bridge_port)
-
-                        #Processing the buffer into our JSON object format
-                        if (len(bufferList)>=(pMQTT-1)):
-                                
-                                payload = DataProcessing().buildPayload()
-
-                                print('Publishing message: \'{}\''.format(payload))
-                                # [START iot_mqtt_jwt_refresh]
-                                seconds_since_issue = (datetime.datetime.utcnow() - jwt_iat).seconds
-                                if seconds_since_issue > 60 * jwt_exp_mins:
-                                        print('Refreshing token after {}s').format(seconds_since_issue)
-                                        jwt_iat = datetime.datetime.utcnow()
-                                        client = self.get_client(
-                                                self.arg_project_id, self.arg_cloud_region,
-                                                self.arg_registry_id, self.arg_device_id, self.arg_private_key_file,
-                                                self.arg_algorithm, self.arg_ca_certs, self.arg_mqtt_bridge_hostname,
-                                                self.arg_mqtt_bridge_port)
-                                # [END iot_mqtt_jwt_refresh]
-                                # Publish "payload" to the MQTT topic. qos=1 means at least once
-                                # delivery. Cloud IoT Core also supports qos=0 for at most once
-                                # delivery.
-                                client.publish(mqtt_topic, payload, qos=1)
-                                bufferList.clear() # This is not supported on older Python versions, only 3.3+
-                        else:
-                                #This is in case it's out of sync, we wait 1 second so it possibly fixes the issue
-                                time.sleep(1)
-                                continue
-                        
-                        # We're publishing the buffer 5 times per minute, to save on queries.
-                        time.sleep(pMQTT)
-
-                print('Finished.')
-        # [END iot_mqtt_run]
 
 class DetectHuman():
         def updateCelVals(self, argCel, argVal):
@@ -350,13 +142,6 @@ class DataProcessing():
         def addToFile(self, filepath, txt):
                 F = open(filepath, 'a')
                 F.write(txt)
-
-        def addToBuffer(self, time, value):
-                global bufferList
-                bufferVal = []
-                bufferVal.extend(value)
-                bufferVal.append(time)
-                bufferList.append(bufferVal)
         
         def buildCsvString(self, time, values):
                 finalString = time + ','
@@ -367,25 +152,6 @@ class DataProcessing():
                                 finalString += str(values[x])
                 finalString += '\n'
                 return finalString
-
-        def buildPayload(self):
-                payload = ''
-                if mode == "full-detail":
-                        for x in range(len(bufferList)):
-                                row = '{'
-                                for y in range(8):
-                                        row = row + '"s'+str(y+1)+'":"'+str(bufferList[x][y])+'",'
-                                row = row + '"time":"' + str(bufferList[x][8]) + '"}'
-                                if (x<len(bufferList)-1):
-                                        row = row + ';'
-                                payload = payload + row
-                else:
-                        for x in range(len(bufferList)):
-                                row = '{"m":"'+str(bufferList[x][0])+'","p":"'+str(bufferList[x][1])+'","t":"'+str(bufferList[x][2])+'"}'
-                                if (x<len(bufferList)-1):
-                                        row = row + ';'
-                                payload = payload + row
-                return payload
 
 class CameraDetection():
         colours = None
@@ -534,7 +300,7 @@ class SerialThread(Thread):
                 if debug:
                     print("Values: {}".format(valsDetail))
                     print("PTAT Value: {}".format(valPTAT))
-
+        conn.close()
 
 
 class DataThread(Thread):
@@ -556,14 +322,6 @@ class DataThread(Thread):
                         if dhCamPresence[x] == 1:
                                 allPresence[x] += 2               
                 
-                #Writing the new data to the Buffer
-                global buffer
-                if buffer:
-                        if mode == "full-detail":
-                                DataProcessing().addToBuffer(st, valsDetail)
-                        else:
-                                DataProcessing().addToBuffer(st, allPresence)
-
                 printVals = []
                 printVals.extend(valsDetail)
                 printVals.append(valPTAT)
@@ -607,15 +365,6 @@ class DetectHumanThread(Thread):
                                 ##
 
                         time.sleep(pLogFile)
-                        
-
-class GCPThread(Thread):
-        def __init__(self):
-                Thread.__init__(self)
-        
-        def run(self):
-                GCloudIOT().main()
-
 
 class CameraThread(Thread):
         def __init__(self):
@@ -644,11 +393,6 @@ if __name__ == '__main__':
                 thread4.setName('Thread 4')
                 thread4.start()
 
-        if mqtt_on:
-                thread5 = GCPThread()
-                thread5.setName('Thread 5')
-                thread5.start()
-        
         #Locking mainthread while thread1 is still alive
         #This means the program won't terminate until thread1 crashes or
         #until we catch the KeyboardInterrupt that will signal
